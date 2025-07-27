9ceec023e2288eb43763c18f8a21376d1cb3f486
Versedcamel153 <seidufarid206@gmail.com>
2025-07-27 22:03:10 +0000

    week 5 fixes

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -3,13 +3,13 @@ from .models import Conversation
 
 class IsParticipantOfConversation(permissions.BasePermission):
     """
-    Custom permission to allow only participants of a conversation
-    to interact with its messages.
+    Allows only participants of a conversation to interact with its messages.
     """
 
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        # Assumes obj has a `conversation` field and `conversation` has `participants`
-        return request.user in obj.conversation.participants.all()
+        if request.method in ["GET", "PUT", "PATCH", "DELETE", "POST"]:
+            return request.user in obj.conversation.participants.all()
+        return False
